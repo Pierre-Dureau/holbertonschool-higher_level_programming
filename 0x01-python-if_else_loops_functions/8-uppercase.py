e6a07b7106ee8116b325a4727dc2d7d9653c8769
Pierre Dureau <dureaupierre53@gmail.com>
2022-04-26 13:07:04 +0200

    Fix format

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def uppercase(str):
-    g = ""
+    s = ""
     for c in str:
         if ord(c) >= 97 and ord(c) <= 122:
             x = ord(c) - 32
         else:
             x = ord(c)
         y = chr(x)
-        g = g + y
-    print(g)
+        s = s + y
+    print("{:s}".format(s))
