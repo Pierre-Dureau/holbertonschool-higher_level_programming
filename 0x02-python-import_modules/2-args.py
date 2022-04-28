9776efa50c0bc68ff4007a1e45b9972c79b7f8f2
Pierre Dureau <dureaupierre53@gmail.com>
2022-04-28 12:12:48 +0200

    Fix for only 1 arg

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,7 +7,10 @@ if __name__ == "__main__":
     if len == 1:
         print("0 arguments.")
     else:
-        print("{:d} arguments:".format(len - 1))
+        if len == 2:
+            print("{:d} argument:".format(len - 1))
+        else:
+            print("{:d} arguments:".format(len - 1))
         for s in argv:
             if i != 0:
                 print("{:d}: {:s}".format(i, s))
