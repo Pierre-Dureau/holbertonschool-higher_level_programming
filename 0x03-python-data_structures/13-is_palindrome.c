4f6c79dc377980ce64d6d5db80ad893618a14e69
Pierre Dureau <dureaupierre53@gmail.com>
2022-05-02 16:25:32 +0200

    Linked list palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,27 +1,6 @@
 #include "lists.h"
 
 /**
- * listint_len - Count the number of elements in a listint_t list
- *
- * @h: Head of the list
- * Return: Number of elements
- */
-
-int listint_len(listint_t *h)
-{
-	int count = 0;
-	listint_t *temp = h;
-
-	while (temp)
-	{
-		temp = temp->next;
-		count++;
-	}
-
-	return (count);
-}
-
-/**
  * is_palindrome - Check if the linked list is a palindrome
  *
  * @head: Head of the list
@@ -30,18 +9,20 @@ int listint_len(listint_t *h)
 
 int is_palindrome(listint_t **head)
 {
-	int length, i = 0, j = 0;
+	int length = 0, i = 0, j = 0;
 	int tab[100];
-	listint_t *temp;
-
-	if (*head == NULL)
-		return (1);
+	listint_t *temp = *head;
 
 	length = listint_len(*head);
-	if (length == 1)
+	if (length < 2)
 		return (1);
 
 	temp = *head;
+	while (temp)
+	{
+		temp = temp->next;
+		length++;
+	}
 
 	while (temp != NULL)
 	{
--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -2,7 +2,6 @@
 #define LISTS_H
 
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
@@ -23,6 +22,5 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
-int listint_len(listint_t *h);
 
 #endif /* LISTS_H */
