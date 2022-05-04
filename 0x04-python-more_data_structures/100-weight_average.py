dda210192fc5e9710716c63b814a434643bcbac6
Pierre Dureau <dureaupierre53@gmail.com>
2022-05-04 19:09:31 +0200

    Weighted average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def weight_average(my_list=[]):
+    if not my_list:
+        return None
+    sum, div = 0, 0
+    for tuple in my_list:
+        sum += tuple[0] * tuple[1]
+        div += tuple[1]
+    return float(sum / div)
