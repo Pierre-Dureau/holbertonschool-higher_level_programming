56145d84aa70fae9d7b5a340a6b0ceb75169dd8e
Pierre Dureau <dureaupierre53@gmail.com>
2022-05-17 18:13:51 +0200

    Bytecodes class

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
+"""Module MagicClass"""
+
+
 import math
 
 
 class MagicClass:
+    """define a Magic Class object"""
+
     def __init__(self, radius):
+        """Initialize radius"""
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
@@ -11,7 +17,9 @@ class MagicClass:
             self.__radius = radius
 
     def area(self):
+        """Calcul area of a circle with a radius"""
         return self.__radius ** 2 * math.pi
 
     def circumference(self):
+        """Calcul circumference of a circle with a radius"""
         return 2 * math.pi * self.__radius
