913a3b5a34cd48fbbad11b28a549842ee3a57297
Pierre Dureau <dureaupierre53@gmail.com>
2022-05-17 17:02:59 +0200

    Singly linked list

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,23 +1,31 @@
 #!/usr/bin/python
-"""Linked List Module"""
+"""
+Linked List Module
+"""
 
 
 class Node:
-    """class Node"""
+    """class creating a Node"""
 
     def __init__(self, data, next_node=None):
-        """Initialize"""
+        """
+        Initialize
+        """
         self.__data = data
         self.__next_node = next_node
 
     @property
     def data(self):
-        """Get data"""
+        """
+        Get data
+        """
         return self.__data
 
     @data.setter
     def data(self, value):
-        """Set data"""
+        """
+        Set data
+        """
         if type(value) is not int:
             raise TypeError("data must be an integer")
         else:
@@ -25,27 +33,37 @@ class Node:
 
     @property
     def next_node(self):
-        """Get next node"""
+        """
+        Get next node
+        """
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
-        """Set next_node"""
-        if type(value) is not Node or value is None:
-            raise TypeError("next_node must be a Node object")
-        else:
+        """
+        Set next_node
+        """
+        if type(value) is Node or value is None:
             self.__next_node = value
+        else:
+            raise TypeError("next_node must be a Node object")
 
 
 class SinglyLinkedList:
-    """class Singly Linked List"""
+    """
+    class creating a Singly Linked List
+    """
 
     def __init__(self):
-        """Initalize"""
+        """
+        Initalize
+        """
         self.__head = None
 
     def sorted_insert(self, value):
-        """Insert node in ascendant order"""
+        """
+        Insert node in ascendant order
+        """
         current = self.__head
 
         if current is None or current.data > value:
@@ -56,7 +74,9 @@ class SinglyLinkedList:
             current.next_node = Node(value, current.next_node)
 
     def __str__(self):
-        """Print every node"""
+        """
+        Print every node
+        """
         current = self.__head
         str_list = ""
 
