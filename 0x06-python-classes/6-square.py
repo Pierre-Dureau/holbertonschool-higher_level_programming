397ddcbc1336c0e6845be8956552585007d9421c
Pierre Dureau <dureaupierre53@gmail.com>
2022-05-17 16:11:59 +0200

    Coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -37,9 +37,9 @@ class Square:
     @position.setter
     def position(self, value):
         """Set the position"""
-        if type(value) is not tuple or len(value) != 2 \
-            or type(value[0]) is not int or type(value[1]) is not int \
-                or value[0] < 0 or value[1] < 0:
+        if type(value) is tuple and len(value) == 2 \
+            and type(value[0]) is int and value[0] >= 0 \
+                and type(value[1]) is int and value[1] >= 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
@@ -51,7 +51,7 @@ class Square:
         else:
             for line in range(self.__position[1]):
                 print()
-            for s in range(self.__size):
+            for siz in range(self.__size):
                 for space in range(self.__position[0]):
                     print(" ", end="")
                 for diez in range(self.__size):
