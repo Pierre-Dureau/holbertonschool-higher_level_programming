2e3695afa523eda01952efec64da8c5449e68cc8
Pierre Dureau <dureaupierre53@gmail.com>
2022-05-17 18:21:53 +0200

    Bytecodes class

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -8,11 +8,10 @@ import math
 class MagicClass:
     """define a Magic Class object"""
 
-    def __init__(self, radius):
+    def __init__(self, radius=0):
         """Initialize radius"""
         self.__radius = 0
-        if type(radius) is not int and type(radius) is not float \
-            and radius is None:
+        if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
         else:
             self.__radius = radius
