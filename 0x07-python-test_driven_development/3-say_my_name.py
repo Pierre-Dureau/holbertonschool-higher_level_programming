f8346c8693f9e0501098f535ce4c861c0fc5be32
Pierre Dureau <dureaupierre53@gmail.com>
2022-05-23 16:35:35 +0200

    Add tests

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    This is the say_my_name Module
+    This module supplies one function say_my_name()
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """Print the first and last name
+        an error will occured if there are not strings"""
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    if len(last_name) == 0:
+        print("My name is", first_name)
+    else:
+        print("My name is", first_name, last_name)
