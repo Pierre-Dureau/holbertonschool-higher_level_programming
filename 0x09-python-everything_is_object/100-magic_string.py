4c55b2a852d456fb8511922638e31c7735a35666
Pierre Dureau <dureaupierre53@gmail.com>
2022-05-24 18:43:01 +0200

    Add files for the project

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string(count=[]):
     count += [1]
-    return ("Holberton, " * len(count))[:-2]
+    return str("Holberton, " * len(count))[:-2]
--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-def copy_list(l):
-    return l.copy()
+def copy_list(list):
+    """Copy a list"""
+    return list.copy()
