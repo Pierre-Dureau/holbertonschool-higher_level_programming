d69ad53dc2ef5447f0e12e097a36fbf85035fa45
Pierre Dureau <dureaupierre53@gmail.com>
2022-05-25 10:25:51 +0200

    Fix numpy function

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -5,12 +5,13 @@
 """
 import numpy as np
 
+
 def lazy_matrix_mul(m_a, m_b):
     """Return the multiplication of two matrix
         or an error if arguments are not int or float
         or if each row of the matrix are not the same size"""
 
     try:
-        return (np.dot(m_a, m_b))
+        return (np.matmul(m_a, m_b))
     except Exception as e:
         print(e)
