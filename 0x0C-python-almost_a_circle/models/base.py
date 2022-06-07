321eede6084b690a48e6a1f4f629f61fce134289
Pierre Dureau <dureaupierre53@gmail.com>
2022-06-07 09:50:52 +0200

    Fix to json string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -23,7 +23,7 @@ class Base():
     @staticmethod
     def to_json_string(list_dictionaries):
         """Get the JSON string representation"""
-        if list_dictionaries is None:
+        if list_dictionaries is None or list_dictionaries == []:
             return []
         else:
             return json.dumps(list_dictionaries)
