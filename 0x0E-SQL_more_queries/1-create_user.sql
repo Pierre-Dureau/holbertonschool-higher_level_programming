31581227d26576d33faa929fa2baa2df35e57e35
Pierre Dureau <dureaupierre53@gmail.com>
2022-07-05 13:37:17 +0200

    creates user and grant all privileges

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- creates the user user_0d_1
+-- if don't exists and grant all privileges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
