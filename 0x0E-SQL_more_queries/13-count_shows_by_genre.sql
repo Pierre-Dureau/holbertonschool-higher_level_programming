767be6992c778361b3539f8635bab6f931e76e7c
Pierre Dureau <dureaupierre53@gmail.com>
2022-07-05 15:50:09 +0200

    count the number of occurence of genre

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- lists all genre
+-- with the number of occurence
+SELECT tv_genres.name, COUNT(tv_show_genres.genre_id) AS number_of_shows
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
