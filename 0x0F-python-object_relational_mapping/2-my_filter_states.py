4a4e5a2d2258149bf9bf31a53723b04bb4b3955c
Pierre Dureau <dureaupierre53@gmail.com>
2022-08-07 15:12:30 +0200

    displays all values where name matches the argument

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,10 +7,10 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3], charset="utf8")
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states  WHERE name = '{}' ORDER BY id ASC"
+                   .format(argv[4]))
     query_rows = cursor.fetchall()
     for row in query_rows:
-        if row[1] == argv[4]:
-            print(row)
+        print(row)
     cursor.close()
     conn.close()
