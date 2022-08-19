1e28e2a12acb351a0716ece572164495d299483c
Pierre Dureau <dureaupierre53@gmail.com>
2022-08-19 12:35:23 +0200

    Get the id of a github user

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Get the id of a github user"""
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    url = 'https://api.github.com/user'
+    r = requests.get(url, auth=(argv[1], argv[2]))
+    r = r.json()
+    print(r['id'])
