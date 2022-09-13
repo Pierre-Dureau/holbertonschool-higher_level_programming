ef8c469f39cd6a4341049b28cf434ababd85f860
Pierre Dureau <dureaupierre53@gmail.com>
2022-09-13 16:04:42 +0200

    read file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const args = process.argv;
+const fs = require('fs');
+
+fs.readFile(args[2], 'utf-8', function(err, data){
+  if (err) {
+    console.log(err)
+  } else {
+    console.log(data);
+  }
+});
