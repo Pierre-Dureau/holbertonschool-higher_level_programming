6aa35ad6ce05fb8062bee0c6ae8ae1b8747a1d94
Pierre Dureau <dureaupierre53@gmail.com>
2022-09-13 17:08:29 +0200

    get method + writefile

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const args = process.argv;
+const axios = require('axios').default;
+const fs = require('fs');
+
+axios.get(args[2]).then(function (response) {
+  fs.writeFile(args[3], response.data, 'utf8', (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
