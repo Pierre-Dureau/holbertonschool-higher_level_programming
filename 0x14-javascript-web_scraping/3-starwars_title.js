db918b094374aba1bd0a89b8be9aec76c810ef07
Pierre Dureau <dureaupierre53@gmail.com>
2022-09-13 16:59:12 +0200

    get method to get data

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,7 +3,6 @@ const args = process.argv;
 const axios = require('axios').default;
 const url = 'https://swapi-api.hbtn.io/api/films/' + args[2];
 
-axios.get(url)
-  .then(function (response) {
-    console.log(response.data.title);
-  });
+axios.get(url).then(function (response) {
+  console.log(response.data.title);
+});
