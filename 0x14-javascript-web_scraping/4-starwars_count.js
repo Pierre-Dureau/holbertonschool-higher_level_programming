59412338b9e7c95e8f284e6cc26607291bf77d68
Pierre Dureau <dureaupierre53@gmail.com>
2022-09-13 17:01:55 +0200

    search value in get method

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,13 +2,14 @@
 const args = process.argv;
 const axios = require('axios').default;
 
-axios.get(args[2])
-  .then(function (response) {
-    let n = 0;
-    response.data.results.forEach(str => {
-      if (str.characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
+axios.get(args[2]).then(function (response) {
+  let n = 0;
+  response.data.results.forEach(film => {
+    film.characters.forEach(char => {
+      if (char.includes('18')) {
         n++;
       }
     });
-    console.log(n);
   });
+  console.log(n);
+});
