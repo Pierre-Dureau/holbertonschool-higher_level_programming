758b5162f49a2898fbc8deee4c2c0aa7f2535bdd
Pierre Dureau <dureaupierre53@gmail.com>
2022-09-13 18:01:51 +0200

    async & await

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,8 +3,8 @@ const args = process.argv;
 const axios = require('axios').default;
 const url = 'https://swapi-api.hbtn.io/api/films/' + args[2];
 
-async function doGetRequest() {
-  const response = await axios.get(url)
+async function doGetRequest () {
+  const response = await axios.get(url);
   for (let i = 0; i < response.data.characters.length; i++) {
     const name = await axios.get(response.data.characters[i]);
     console.log(name.data.name);
